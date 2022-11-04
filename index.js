f9aa376d70036ecd245ab207ddd385107f48ce63
Nicerrika <runkai.qiu@stonybrook.edu>
2022-11-04 16:32:28 -0400

    test

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 // PUT THE SERVER IN LISTENING MODE
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-app.use(express.static("client"));
-// app.get('*', function (req, res){
-//     res.sendFile(path.join(__dirname + '/', 'client', 'index.html'));
-// });
+app.use(express.static("client/build"));
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 
